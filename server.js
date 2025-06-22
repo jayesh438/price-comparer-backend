dd61c35aa82b76526331079d792688d713acc9f9
jayesh438 <jayeshamilineni24@gmail.com>
2025-06-22 13:06:04 +0530

    Update server.js

--- server.js
+++ server.js
@@ -10,7 +10,11 @@ const PORT = process.env.PORT || 3000;
 
 app.post('/compare-prices', async (req, res) => {
   const { productName } = req.body;
-  const browser = await puppeteer.launch({ headless: true });
+  const browser = await puppeteer.launch({
+    headless: true,
+    args: ['--no-sandbox', '--disable-setuid-sandbox']
+  });
+
   const results = [];
 
   try {
